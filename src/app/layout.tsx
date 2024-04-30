ee5cf80df40be97d79c9912a12dce484f64b752a
Chase Albritton <59696546+stunnas@users.noreply.github.com>
2024-04-30 13:54:24 -0400

    logo update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <link
+          rel="icon"
+          href="/wordassistant.png"
+          sizes="any"
+        />
+      </body>
     </html>
   );
 }
